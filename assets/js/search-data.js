831b0774673e5fd14e296b8503132bd7b6399ed1
aerosolncloud <praveen07ec@gmail.com>
2026-03-20 18:22:11 +0000

    Deploying to gh-pages from @ aerosolncloud/aerosolncloud.github.io@16d1d75da855aed73543b9978c935390db41524a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
